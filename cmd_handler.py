736f45d1dcb336ad466250eb9d390fbf91d9beef
Stein Magnus Jodal <stein.magnus@jodal.no>
2009-01-11 23:58:38 +0100

    Objectify

--- cmd_handler.py
+++ cmd_handler.py
@@ -2,22 +2,36 @@
 
 import sys
 
-def main():
-    if len(sys.argv) < 2:
-        print 'Too few arguments: %s' % sys.argv
+class Blackcat(object):
+    def __init__(self):
+        if len(sys.argv) < 2:
+            sys.exit('Too few arguments')
 
-    input = sys.argv[1]
-    tokens = input.split(' ')
-    nick = tokens[0]
-    channel = tokens[1]
-    sender = tokens[2]
-    first = tokens[3]
-    rest = ' '.join(tokens[4:])
+        tokens = sys.argv[1].split(' ')
+        self.nick = tokens[0]
+        self.channel = tokens[1]
+        self.sender = tokens[2]
+        self.command = tokens[3]
+        self.message = ' '.join(tokens[3:])
 
-    if first == 'hi':
-        print 'Hi, %s! How you doing?' % nick
-    else:
-        print '%s: Dunno. Fork http://code.jodal.no/git/?p=blackcat.git and fix it.' % nick
+        self.handle_message()
+
+    def handle_message(self):
+        # TODO Replace with list of regexps and handlers
+        if self.command == 'hi':
+            self.handle_hi()
+        else:
+            self.handle_unknown()
+
+    def out(self, text):
+        # TODO Add nick, channel, etc. automatically
+        print text
+
+    def handle_hi(self):
+        self.out('Hi, %s! How you doing?' % self.nick)
+
+    def handle_unknown(self):
+        self.out('%s: Dunno. Fork http://code.jodal.no/git/?p=blackcat.git and fix it.' % self.nick)
 
 if __name__ == '__main__':
-    main()
+    blackcat = Blackcat()
