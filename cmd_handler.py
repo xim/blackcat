a0a3d5d2acf2f7bab14e18b8265f60b28e363ec0
Kristian Klette <klette@Hatred.lan>
2009-01-12 00:34:56 +0100

    Implemented rss-support - merge..

--- cmd_handler.py
+++ cmd_handler.py
@@ -4,24 +4,6 @@ import sys
 import pickle
 import datetime
 
-<<<<<<< HEAD:cmd_handler.py
-def main():
-    if len(sys.argv) < 2:
-        print 'Too few arguments: %s' % sys.argv
-
-    input = sys.argv[1]
-    tokens = input.split(' ')
-    nick = tokens[0]
-    channel = tokens[1]
-    sender = tokens[2]
-    first = tokens[3]
-    rest = ' '.join(tokens[4:])
-
-    if first == 'hi':
-        print 'Hi, %s! How you doing?' % nick
-    else:
-        print '%s: Dunno. Fork http://code.jodal.no/git/?p=blackcat.git and fix it.' % nick
-=======
 class Blackcat(object):
     def __init__(self):
         if len(sys.argv) < 2:
